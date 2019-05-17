6ff98286e5bfa4e34b0225e6b45beacbb9485ff6
Gusti Made Adrian Putradinata <49512467+natenz@users.noreply.github.com>
2019-05-17 10:09:06 +0700

    Add files via upload

---
+++ MyConnection.java
@@ -0,0 +1,23 @@
+package tubespbo;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class MyConnection {
+    public static Connection getConnection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/tubess","root","");
+            
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());        
+        }
+        return con;
+    }
+}
